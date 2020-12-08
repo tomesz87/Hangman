08ff2a6ecd08205433ee2bcb6fdb1c0740374acc
tomesz87 <67005333+tomesz87@users.noreply.github.com>
2020-12-09 00:10:58 +0100

    change
    Some minor wording

--- task/hangman/hangman.py
+++ task/hangman/hangman.py
@@ -10,14 +10,12 @@ while choice.lower() not in ['play', 'exit']:
     choice = input('Type "play" to play the game, "exit" to quit:')
 
 if choice.lower() == 'exit':
-    sys.exit(0)
+    end(0)
 
 list_available = ['python', 'java', 'kotlin', 'javascript']
 to_guess = random.choice(list_available)
 current = '-' * len(to_guess)
 tries = 8
-
-already_typed = set()
 
 letters = set(to_guess)
 while tries > 0:
@@ -26,18 +24,6 @@ while tries > 0:
     # print('Input a letter:')
     letter = input('Input a letter:')
 
-    if len(letter) != 1:
-        print('You should input a single letter')
-        continue
-
-    if letter not in set('abcdefghijklmnopqrtsuvwxyz'):
-        print('It is not an ASCII lowercase letter')
-        continue
-
-    if letter in already_typed:
-        print("You already typed this letter")
-        continue
-
     if letter in letters:
         letters.discard(letter)
         if len(letters) == 0:
@@ -45,10 +31,13 @@ while tries > 0:
         current = to_guess
         for c in letters:
             current = current.replace(c, "-")
+    elif letter in set(to_guess):
+        print("No improvements")
+        tries -= 1
     else:
         print("No such letter in the word")
         tries -= 1
-    already_typed.add(letter)
+
 if tries > 0:
     print("You survived!")
 else:
